f3545dcaf7858252779a041226ac87bc4f19ccbe
Yuri Silva <121751241+yuribertozzi@users.noreply.github.com>
2023-01-03 12:00:43 -0300

    Add files via upload

---
+++ apito.py
@@ -0,0 +1,8 @@
+tempodejogo = int(input("Quanto tempo temos já jogado?: "))
+
+if tempodejogo <= 90:
+	print("Ainda tem jogo pela frente.")
+	print("Que bom, eu adoro futebol!")
+else:
+	print("Putz, tá acabando!")
+	print("Apita logo, juiz!")
